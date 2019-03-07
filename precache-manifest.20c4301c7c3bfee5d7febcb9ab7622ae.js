9898e0694840e02937b3a1577cf194b174d049bd
Knyazev Ivan <to.knyazev@gmail.com>
2019-03-07 21:52:02 +0300

    new

---
+++ precache-manifest.20c4301c7c3bfee5d7febcb9ab7622ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "0971a52b8983d4d152ca",
+    "url": "/static/js/main.0971a52b.chunk.js"
+  },
+  {
+    "revision": "ac164eeb7fe39897100f",
+    "url": "/static/js/2.ac164eeb.chunk.js"
+  },
+  {
+    "revision": "0971a52b8983d4d152ca",
+    "url": "/static/css/main.207f6fb0.chunk.css"
+  },
+  {
+    "revision": "7395161cb04328716e48583decf3c9de",
+    "url": "/index.html"
+  }
+];
